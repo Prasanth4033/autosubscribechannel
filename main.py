ed0d039c207bc44ce36c09e477018a860ecf9d73
prasanth4033 <gspr@gmail.com>
2022-07-04 22:34:55 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from bot_studio import *
+
+youtube=bot_studio.youtube()
+youtube.login(username='username',password='password')
+youtube.auto_subscribe(channel_url="https://www.youtube.com/channel/UCrTuibZfdGjnTs5lCc1QpvQ")
+
+
